ac36a56d20892aa59b30a5d39ea665e48b3093a8
醉樱桃 <mtomic2020@gmail.com>
2024-12-25 02:28:56 +0300

    disabled explore button

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,3 @@
 function setMobileReleaseDisabled(qualifiedName, value) {
-    document.getElementById("mobile-release").setAttribute("disabled", "")
-    document.getElementById("mobile-beta").setAttribute("disabled", "")
-    document.getElementById("mobile-release-pn").textContent = "will be available later"
-    document.getElementById("mobile-beta-pn").textContent = "will be available later"
+    document.getElementById("explore").disabled = true
 }
